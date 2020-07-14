1722a9e6b6a78a3b3b5e80d44aa49802ab82568c
KCY <ghkdua059@gmail.com>
2020-07-15 00:05:39 +0900

    [베이스액티비티] 상태바 없애는 베이스 액티비티 생성

--- app/src/main/java/com/bibim/purpur/base/BaseActivity.kt
+++ app/src/main/java/com/bibim/purpur/base/BaseActivity.kt
@@ -1,19 +1,22 @@
 package com.bibim.purpur.base
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatActivity() {
-    lateinit var viewDataBinding: T
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+    private lateinit var viewDataBinding: T
 
     abstract val layoutResID: Int
-    abstract val viewModel: R
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         // 초기화된 layoutResId로 databinding 객체 생성
         viewDataBinding = DataBindingUtil.setContentView(this, layoutResID)
         // live data를 사용하기 위해 해줘야함
