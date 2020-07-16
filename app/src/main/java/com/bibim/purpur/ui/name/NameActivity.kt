4e7f8128c0afc980ff0f75741639d7ccea50e3c0
KCY <ghkdua059@gmail.com>
2020-07-16 18:02:27 +0900

    [이름설정뷰] 로직 및 뷰 추가

--- app/src/main/java/com/bibim/purpur/ui/name/NameActivity.kt
+++ app/src/main/java/com/bibim/purpur/ui/name/NameActivity.kt
@@ -2,11 +2,17 @@ package com.bibim.purpur.ui.name
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bibim.purpur.R
+import com.bibim.purpur.base.BaseActivity
+import com.bibim.purpur.databinding.ActivityNameBinding
 
-class NameActivity : AppCompatActivity() {
+class NameActivity : BaseActivity<ActivityNameBinding>() {
+    override val layoutResID: Int
+        get() = R.layout.activity_name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_name)
+
+        
     }
 }
