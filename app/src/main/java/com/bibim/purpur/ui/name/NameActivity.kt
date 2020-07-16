766109e1d3101dc435f6b7e2d0f745b9efa7076e
KCY <ghkdua059@gmail.com>
2020-07-16 18:02:08 +0900

    [추가] 한번만 클릭 추가 및 설정 수정

---
+++ app/src/main/java/com/bibim/purpur/ui/name/NameActivity.kt
@@ -0,0 +1,12 @@
+package com.bibim.purpur.ui.name
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+    }
+}
