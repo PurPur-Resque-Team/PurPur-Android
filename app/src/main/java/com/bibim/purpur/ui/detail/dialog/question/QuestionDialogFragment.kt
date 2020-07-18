740a7a90bd542b3eb2632a7e32e2a3fe01b97800
KCY <ghkdua059@gmail.com>
2020-07-18 16:17:18 +0900

    [질문 다이얼로그] 물음표 창 다이얼로그 구현

--- app/src/main/java/com/bibim/purpur/ui/detail/dialog/question/QuestionDialogFragment.kt
+++ app/src/main/java/com/bibim/purpur/ui/detail/dialog/question/QuestionDialogFragment.kt
@@ -1,19 +1,20 @@
 package com.bibim.purpur.ui.detail.dialog.question
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.bibim.purpur.databinding.DialogQuestionBinding
-import com.bibim.purpur.ui.detail.dialog.quiz.QuizDialogFragment
 
 class QuestionDialogFragment : DialogFragment() {
     private lateinit var binding: DialogQuestionBinding
 
-    fun getInstance(): QuizDialogFragment {
-        return QuizDialogFragment()
+    fun getInstance(): QuestionDialogFragment {
+        return QuestionDialogFragment()
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -24,12 +25,22 @@ class QuestionDialogFragment : DialogFragment() {
         }
     }
 
+    private fun setBackground() {
+        // 배경에 희게 각지게 나오는 거 방지.
+        with(dialog!!.window!!) {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DialogQuestionBinding.inflate(LayoutInflater.from(this.context), container, false)
+
+        setBackground()
+
         return binding.root
     }
 }
